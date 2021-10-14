1d680341e40a053fe0524e571dd5d097f77fb5ee
Louis Frament <lf@novel-t.ch>
2021-10-14 13:25:56 +0200

    xmart context and env credentials

--- panther/src/tests/SimpleBrowserTest.php
+++ panther/src/tests/SimpleBrowserTest.php
@@ -9,64 +9,95 @@ use Symfony\Component\Panther\PantherTestCase;
 
 class SimpleBrowserTest extends PantherTestCase
 {
-    const USERNAME = '';
-    const PASSWORD = '';
+    private $USERNAME = null;
+    private $PASSWORD = null;
+    static $client = null;
 
-    public function testNovelT(): void
-    {
-        $client = Client::createChromeClient();
+    public function __construct(?string $name = null, array $data = [], $dataName = '') {
+        parent::__construct($name, $data, $dataName);
 
-        $client->request('GET', 'https://novel-t.ch/');
-        $client->waitFor('li:nth-of-type(3) > .nav-link');
-        $client->takeScreenshot('page-homepage.png');
-        $client->clickLink('Our Focus');
-
-        // Wait for an element to be present in the DOM (even if hidden)
-        $crawler = $client->waitForElementToContain('.header-title','Our Focus');
-
-        $headerTitle = $crawler->filter('.header-title')->text();
-        $this->assertEquals("Our Focus", $headerTitle);
-        $client->takeScreenshot('page-our-focus.png');
-
-        $crawler = $client->waitFor('app-focus > .our-focus-landing');
-
-        $crawler->filter('div:nth-of-type(3) > div:nth-of-type(2) > .solution-part-2 > .solution-2')->click();
-        $client->takeScreenshot('page-integration.png');
-
-        $p = $crawler->filter('.focus-para > p')->text();
-        $this->assertEquals('Power knowledge-driven decisions with integrated, centralized and harmonized data', $p);
+        $this->USERNAME = getenv('AD_USERNAME');
+        $this->PASSWORD = getenv('AD_PASSWORD');
     }
 
-    public function testXmart(): void
+    public function setUp(): void
     {
-        $client = Client::createChromeClient();
+        $pantherClient = self::$pantherClient;
 
-        $client->request('GET', 'https://extranet-uat.novel-t.ch/xmart/');
-        $client->waitForVisibility("input[name='loginfmt']");
-        $client->takeScreenshot('1-login.png');
-        $crawler = $client->getCrawler();
-        $crawler->filter("input[name='loginfmt']")->sendKeys(self::USERNAME);
-        $client->takeScreenshot('2-loginUsernameFilled.png');
+        if ($pantherClient === null) {
+            $this->createAndLoginPanther();
+        }
+
+        parent::setUp();
+    }
+
+    private function createAndLoginPanther()
+    {
+        $pantherClient = static::createPantherClient(
+        [
+            'external_base_uri' => 'https://extranet-uat.novel-t.ch/xmart',
+            'request_timeout_in_ms' => 60000
+        ],
+        [],
+        [
+            'capabilities' => [
+                'acceptInsecureCerts' => true,
+            ],
+            'request_timeout_in_ms' => 20000000
+        ]
+        );
+
+        self::$pantherClient->request('GET', '/');
+        
+        self::$pantherClient->waitForVisibility("input[name='loginfmt']");
+        self::$pantherClient->takeScreenshot('1-login.png');
+        $crawler = self::$pantherClient->getCrawler();
+        $crawler->filter("input[name='loginfmt']")->sendKeys($this->USERNAME);
+        self::$pantherClient->takeScreenshot('2-loginUsernameFilled.png');
         $crawler->filter("input#idSIButton9")->click();
 
-        $client->waitForVisibility("input[name='passwd']");
-        $client->takeScreenshot('3-loginUsernameDone.png');
+        self::$pantherClient->waitForVisibility("input[name='passwd']");
+        self::$pantherClient->takeScreenshot('3-loginUsernameDone.png');
 
-        $crawler = $client->getCrawler();
-        $crawler->filter("input[name='passwd']")->sendKeys(self::PASSWORD );
+        $crawler = self::$pantherClient->getCrawler();
+        $crawler->filter("input[name='passwd']")->sendKeys($this->PASSWORD );
         $crawler->filter("input#idSIButton9")->click();
 
-        $client->waitForVisibility("input#idBtn_Back");
-        $client->takeScreenshot('4-loginPassword.png');
-        $crawler = $client->getCrawler();
+        self::$pantherClient->waitForVisibility("input#idBtn_Back");
+        self::$pantherClient->takeScreenshot('4-loginPassword.png');
+        $crawler = self::$pantherClient->getCrawler();
         $crawler->filter("input#idBtn_Back")->click();
 
-        $client->takeScreenshot('loginNo.png');
-        $client->waitForVisibility("mart-figures-card h4");
-        $client->takeScreenshot('5-logged.png');
+        self::$pantherClient->takeScreenshot('loginNo.png');
+        self::$pantherClient->waitForVisibility("mart-figures-card h4");
+        self::$pantherClient->takeScreenshot('5-logged.png');
+    }
 
-        $crawler = $client->getCrawler();
+    public function testXmartHome(): void
+    {
+
+        self::$pantherClient->request('GET', '/');
+
+        self::$pantherClient->waitForVisibility("mart-figures-card h4");
+        self::$pantherClient->takeScreenshot('5-testXmartHome_logged.png');
+
+        $crawler = self::$pantherClient->getCrawler();
         $xMart = $crawler->filter(".p-col-12.p-md-6.xmart-page-titles > div > p:nth-of-type(1)")->getText();
         $this->assertEquals('xMart is a self-service platform for data managers.', $xMart);
+    }
+
+    
+    public function testXmartWisse(): void
+    {
+
+        self::$pantherClient->request('GET', '/WIISE');
+
+        
+        self::$pantherClient->waitForVisibility("mart-figures-card h4");
+        self::$pantherClient->takeScreenshot('5-testXmartWiise_logged.png');
+
+        $crawler = self::$pantherClient->getCrawler();
+        $xMartTitle = $crawler->filter(".mart-title a")->getText();
+        $this->assertEquals('Wiise Mart', $xMartTitle);
     }
 }
