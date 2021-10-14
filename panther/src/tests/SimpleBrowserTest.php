62ea3be9fb2a39159ee03be26a6ddd699b948490
Jean-Pierre Lambelet <jpl@novel-t.ch>
2021-10-14 11:10:18 +0200

    adding testXmart

--- panther/src/tests/SimpleBrowserTest.php
+++ panther/src/tests/SimpleBrowserTest.php
@@ -9,6 +9,9 @@ use Symfony\Component\Panther\PantherTestCase;
 
 class SimpleBrowserTest extends PantherTestCase
 {
+    const USERNAME = '';
+    const PASSWORD = '';
+
     public function testNovelT(): void
     {
         $client = Client::createChromeClient();
@@ -32,5 +35,38 @@ class SimpleBrowserTest extends PantherTestCase
 
         $p = $crawler->filter('.focus-para > p')->text();
         $this->assertEquals('Power knowledge-driven decisions with integrated, centralized and harmonized data', $p);
+    }
+
+    public function testXmart(): void
+    {
+        $client = Client::createChromeClient();
+
+        $client->request('GET', 'https://extranet-uat.novel-t.ch/xmart/');
+        $client->waitForVisibility("input[name='loginfmt']");
+        $client->takeScreenshot('1-login.png');
+        $crawler = $client->getCrawler();
+        $crawler->filter("input[name='loginfmt']")->sendKeys(self::USERNAME);
+        $client->takeScreenshot('2-loginUsernameFilled.png');
+        $crawler->filter("input#idSIButton9")->click();
+
+        $client->waitForVisibility("input[name='passwd']");
+        $client->takeScreenshot('3-loginUsernameDone.png');
+
+        $crawler = $client->getCrawler();
+        $crawler->filter("input[name='passwd']")->sendKeys(self::PASSWORD );
+        $crawler->filter("input#idSIButton9")->click();
+
+        $client->waitForVisibility("input#idBtn_Back");
+        $client->takeScreenshot('4-loginPassword.png');
+        $crawler = $client->getCrawler();
+        $crawler->filter("input#idBtn_Back")->click();
+
+        $client->takeScreenshot('loginNo.png');
+        $client->waitForVisibility("mart-figures-card h4");
+        $client->takeScreenshot('5-logged.png');
+
+        $crawler = $client->getCrawler();
+        $xMart = $crawler->filter(".p-col-12.p-md-6.xmart-page-titles > div > p:nth-of-type(1)")->getText();
+        $this->assertEquals('xMart is a self-service platform for data managers.', $xMart);
     }
 }
